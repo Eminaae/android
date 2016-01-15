fd65e092e3a98920e4fc31f38eba09bb796c3a32
unknown <emina.arapcic@bitcamp.ba>
2016-01-15 15:09:31 +0100

    welcome screen

---
+++ LeftorTest/app/src/main/java/ba/leftor/exercises/leftortest/ToDoListActivity.java
@@ -0,0 +1,23 @@
+package ba.leftor.exercises.leftortest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by USER on 15.1.2016.
+ */
+public class ToDoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todo_list);
+
+    }
+
+
+
+
+}
--- LeftorTest/app/src/main/java/ba/leftor/exercises/leftortest/WelcomeScreenActivity.java
+++ LeftorTest/app/src/main/java/ba/leftor/exercises/leftortest/WelcomeScreenActivity.java
@@ -1,13 +1,29 @@
 package ba.leftor.exercises.leftortest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class WelcomeScreenActivity extends AppCompatActivity {
+public class WelcomeScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        TextView textView = (TextView)findViewById(R.id.welcome);
+        textView.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view){
+        TextView textView = (TextView)findViewById(R.id.welcome);
+        String text = textView.getText().toString();
+        Intent intent = new Intent(this, ToDoListActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
